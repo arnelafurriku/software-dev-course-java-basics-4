b55006be70316f8e31992121618f7a89d33f158a
Arnela Cuci <40432206+arnelafurriku@users.noreply.github.com>
2025-12-15 17:17:23 -0600

    Added example exercise solutions.

--- src/main/java/org/example/StudentManager.java
+++ src/main/java/org/example/StudentManager.java
@@ -9,11 +9,12 @@ public class StudentManager {
     private List<String> students = new ArrayList<>();
 
     public void mainMenu() {
-        System.out.println("Welcome to the student manager!");
+        System.out.println("\nWelcome to the student manager!");
         System.out.println("1. Add student");
         System.out.println("2. Remove student");
         System.out.println("3. View all students");
         System.out.println("4. Exit");
+        System.out.print("Enter your choice: ");
 
         String choice = scanner.nextLine();
 
@@ -28,28 +29,45 @@ public class StudentManager {
             System.exit(0);
         } else {
             System.out.println("Invalid choice. Please try again.");
+            mainMenu();
         }
     }
 
     public void addStudent() {
-        // Prompt the user to enter a new student name (using scanner and I/O methods learned previously,
-        // refer to mainMenu() for an example)
-        // Add the student to the list
+        System.out.print("Enter the new student's name: ");
+        String studentName = scanner.nextLine();
+
+        students.add(studentName);
+        System.out.println(studentName + " added.");
 
         mainMenu();
     }
 
     public void removeStudent() {
-        // Prompt the user for a student name
-        // Use the contains method to check if the student entered is in the list
-        // If so, remove it, if not, print "Student not found."
+        System.out.print("Enter the student's name to remove: ");
+        String studentName = scanner.nextLine();
+
+        if (students.contains(studentName)) {
+            students.remove(studentName);
+            System.out.println(studentName + " removed.");
+        } else {
+            System.out.println("Student not found.");
+        }
 
         mainMenu();
     }
 
     public void viewStudents() {
-        // Loop through the list of students and print each one
-        // (Use a for-each loop!)
+        System.out.println("\nStudents:");
+
+        if (students.isEmpty()) {
+            System.out.println("(No students yet)");
+        } else {
+            for (String student : students) {
+                System.out.println("- " + student);
+            }
+        }
+
         mainMenu();
     }
 }
